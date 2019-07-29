b535e27663eb8bad2ecad33ebe71d40a47c9e523
Farkhod <sadykovfarkhod@gmail.com>
2019-07-28 23:03:02 -0500

    Updated variables files

--- kube-cluster/google_bucket.tf
+++ kube-cluster/google_bucket.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "fuchicorp_bucket" {
-  name   = "fuchicorp-bucket"
+  name   = "${var.bucket_name}"
   storage_class = "COLDLINE"
   location = "${var.region}"
 }
--- kube-cluster/variables.tf
+++ kube-cluster/variables.tf
@@ -1,15 +1,23 @@
 variable "cluster_name" {
   default = "fuchicorp-cluster"
 }
+
 variable "node_count" {
   default = "2"
 }
+
 variable "project" {
   default = "universal-team-245120"
 }
+
 variable "service_account_path" {
   default = "./fuchicorp-service-account.json"
 }
+
 variable "region" {
   default = "us-central1"
+}
+
+variable "bucket_name" {
+  default = "fuchicorp-bucket"
 }
