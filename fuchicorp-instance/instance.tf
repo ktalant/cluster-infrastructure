a36f8aacc3d4491741d9f6d8a636a22d0293f48e
Caglar Cavdar <chaglare@gmail.com>
2019-08-12 17:13:31 -0500

    instance.tf added

---
+++ fuchicorp-instance/instance.tf
@@ -0,0 +1,15 @@
+data "google_compute_image" "my_image" {
+  family  = "centos-7"
+  project = "fuchicorp"
+}
+
+resource "google_compute_instance_template" "instance_template" {
+  name_prefix  = "instance-template-"
+  machine_type = "n1-standard-1"
+  region       = "us-central1"
+
+  // boot disk
+  disk {
+    source_image = "${google_compute_image.my_image.self_link}"
+  }
+}
