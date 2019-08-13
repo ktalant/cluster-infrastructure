1753c4135ff8a503e37707568f9e8e13f9fc71a0
Caglar Cavdar <chaglare@gmail.com>
2019-08-12 19:06:43 -0500

    edited

--- fuchicorp-instance/main.tf
+++ fuchicorp-instance/main.tf
@@ -2,13 +2,10 @@ resource "google_compute_instance_template" "bastion" {
   name                = "${var.instance_name}"
   machine_type        = "${var.machine_type}"
   project             = "${var.project}"
+  region              = "${var.region}"
   count               = "${var.count}"
   # name  = "${var.name_prefix}-${count.index}"
 
-data "google_compute_zones" "available" {
-  region = "${var.region}"
-  status = "UP"
-}
 
   // boot disk
   disk {
