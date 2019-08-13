13abd3cff2852eba3f13e63e5d0edeb023aaf649
Caglar Cavdar <chaglare@gmail.com>
2019-08-12 19:45:38 -0500

    edited

--- fuchicorp-instance/main.tf
+++ fuchicorp-instance/main.tf
@@ -10,6 +10,14 @@ resource "google_compute_instance_template" "vm_instance" {
   
   count               = "${var.count}"
   # name  = "${var.name_prefix}-${count.index}"
+
+    // Use an existing disk resource
+  disk {
+    // Instance Templates reference disks by name, not self link
+    source      = "${google_compute_disk.bastion.name}"
+    auto_delete = false
+    boot        = false
+  }
  
 
   network_interface {
