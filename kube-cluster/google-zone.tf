7c9bd0f4987325d225fc01df5d05b3f5843748c3
ktalant <uchkunfan@gmail.com>
2020-03-03 10:42:33 -0500

    updated

--- kube-cluster/google-zone.tf
+++ kube-cluster/google-zone.tf
@@ -1,5 +1,5 @@
-resource "google_dns_managed_zone" "fuchicorp" {
-  name     = "fuchicorp"
-  dns_name = "fuchicorp.com."
+resource "google_dns_managed_zone" "talantzon" {
+  name     = "talantzon"
+  dns_name = "talantzon.com."
   project  = "${var.google_project_id}"
 }
--- kube-cluster/kubecluster-deployment.tf
+++ kube-cluster/kubecluster-deployment.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = "${file("./fuchicorp-service-account.json")}" #GOOGLE_CREDENTIALS to the path of a file containing the credential JSON
+  credentials = "${file("./talant-account.json")}" #GOOGLE_CREDENTIALS to the path of a file containing the credential JSON
   project     = "${var.google_project_id}"
 }
 
